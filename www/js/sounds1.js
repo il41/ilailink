47d1bffce120f321aa9d886ff2649579fc13faa9
il41 <igilbe1@artic.edu>
2019-05-20 18:20:28 -0500

    added play button and speedometer

--- www/js/sounds1.js
+++ www/js/sounds1.js
@@ -1,17 +1,50 @@
+let displayDiv;
+let playDiv;
+
+
 function preload() {
   soundFormats('mp3', 'ogg');
   mySound = loadSound('audio/20191/be.mp3');
 }
 
+
 function setup() {
+  playDiv = document.createElement('div');
+  document.body.appendChild(playDiv);
+  playDiv.id = "playButton";
+  playDiv.innerHTML = "play! (click wave to change speed)";
+  playDiv.style.cursor = "crosshair";
+
+  playDiv.addEventListener("click", () => {
+    mySound.play();
+    playDiv.style.display = "none";
+    displayDiv = document.createElement('div');
+    document.body.appendChild(displayDiv);
+    displayDiv.id = "speedometer";
+    displayDiv.innerHTML = mySound.getPlaybackRate();
+    displayDiv.style.cursor = "crosshair";
+  });
+
+
+
   mySound.rate(0.7);
   mySound.setVolume(1);
-  mySound.play();
+  //mySound.play();
   fft = new p5.FFT();
   createCanvas(200, 200);
   background(255, 0, 200);
-
+  textSize(32);
+  text('click me', 10, 30);
 }
+
+addEventListener("click", () => {
+  displayDiv.innerHTML = mySound.getPlaybackRate();
+});
+
+
+
+
+
 
 function draw(){
   let theDiv = document.getElementById('yo');
