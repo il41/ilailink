7c7bad03a9a63d8545bb43715ebf519f5e004841
il41 <ilai.gilbert@gmail.com>
2019-09-06 01:17:16 -0500

    added temporal

---
+++ www/js/temporal.js
@@ -0,0 +1,111 @@
+let iw = innerWidth;
+let ih = innerHeight;
+let mysound;
+let fft = new p5.FFT();
+let canv;
+let startVar = 1;
+
+function preload() {
+  soundFormats('mp3');
+  mysound = loadSound('audio/illegalsubpassracing.mp3');
+  mysound.setVolume(1);
+  mysound.rate(0.7);
+}
+
+
+function setup(){
+  createCanvas(innerWidth,innerHeight);
+  // triangle(iw/2, ih/2, iw/2+iw/7, ih-ih/3, iw/2, ih/2+ih/3);
+  noLoop();
+  canv = document.getElementById('defaultCanvas0');
+  console.log(canv);
+  canv.addEventListener('click',()=>{
+    if(startVar==1){
+      loop();
+      mysound.play();
+      background(255,255,255);
+    }
+    startVar=0;
+  });
+
+
+}
+
+// function start(){
+//   loop();
+//   mysound.play();
+//   canv.removeEventListener('click');
+// }
+
+
+// function drawCircle(x, y, radius, i1, i2) {
+//   for (let j = 0; j <= 360; j++) {
+//     rect(x, y, cos(j) * radius+i1, sin(j) * radius+i2);
+//   }
+// }
+
+
+let i = 0;
+let j = 0;
+
+function draw(){
+  // rotate(j);
+  // noFill();
+  let spectrum = fft.analyze();
+  // stroke('black');
+    if(i>innerWidth/2){
+      i=0;
+    }
+
+    i+=1;
+    // let low = spectrum[spectrum.length/2/2];
+    let low = spectrum[50]*2;
+    let mid = spectrum[spectrum.length/2]*2;
+    let high = spectrum[spectrum.length/2*1.5]*2;
+    let amp = spectrum.reduce((a,b)=>{
+      return a+b;
+    });
+    // console.log(spectrum);
+    stroke(color(mid,high,low));
+    fill(mid,high,low);
+
+    strokeWeight(15);
+
+
+    // drawCircle(innerWidth/2,innerHeight/2,i,low,mid);
+
+let cr = 20;
+
+    if(j>=360){
+      j=0;
+    }
+    let x =849;
+    let y =318;
+    // rect(cos(j)/2*i+x, sin(j)/2*i+y,i/20,i/20);
+
+    j+=1.2;
+
+
+
+    // background(color(mid*2,Math.abs(high-100*200),low,1));
+    noFill();
+    strokeWeight(i/1000);
+
+    low = spectrum[10]*2;
+    stroke(0,0,low);
+    rotate(i);
+    translate(j,j);
+    quad(0,height,width,height,width,0,-1000,0);
+    rotate(i);
+
+
+    // background(color(mid/255,high/255,low/255,1));
+    //stroke(map(spectrum[spectrum.length/2], 0, 255, height, 0))
+    // stroke(spectrum.length[spectrum.length/2/2],spectrum.length[spectrum.length/2],spectrum.length[spectrum.length/2*1.5]); // spectrum is green
+  // for (let j = 0; j< spectrum.length; j++){
+  //   let x = map(i, 0, spectrum.length, 0, width);
+  //   let h = -height + map(spectrum[j], 0, 255, height, 0);
+  //   rect(x, height, width / spectrum.length, h )
+  // }
+
+}
