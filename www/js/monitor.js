5b1a9b4c99c3fddc3d0866dba35e3f7d48f2cefc
il41 <ilai.gilbert@gmail.com>
2019-09-15 17:58:28 -0500

    integrated monitors on click

--- www/js/monitor.js
+++ www/js/monitor.js
@@ -11,7 +11,7 @@ function setup(){
 function draw(){
   background(0,0,0,1)
   // stroke('black');
-  noStroke();
+
     if(x>innerWidth*1.1){
       x=0;
     }
@@ -19,5 +19,15 @@ function draw(){
     x+=2;
 
     fill(0,255,0); // spectrum is green
+    noStroke();
     rect(x, innerHeight, 1, -map(Math.random(),0,1,0,innerHeight) );
+}
+function mouseDragged() {
+  triangle(0,0,mouseX, mouseY, Math.random()*innerWidth, Math.random()*innerHeight);
+  // prevent default
+  return false;
+}
+function mouseClicked() {
+  window.open("monitors.html",'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=500,height=500,top=500em');
+
 }
