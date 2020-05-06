11218d2caa79aca291e22362a3a490c6a540c9e2
il41 <igilbe1@artic.edu>
2020-05-06 03:44:22 -0700

    added ds2 body

---
+++ www/js/body.js
@@ -0,0 +1,198 @@
+//needs
+
+// let major = [43,45,48,50,51,52,54,55,57,60,62,63,64,66,67,69,72,74,75,76,78,79,81,84,86,87,88,90,91,93,96,98];
+// let minor = [51,53,55,56,57,58,59,60,62,63,65,67,68,69,70,71,72,74,75,77,79,80,81,82,83,84,86,87,89,91,92,93];
+let cMajPentMIDI = [52,55,57,60,62,64,67,69,72,74,76,79,81,84,86,88];
+let cMajPent = []
+// let dMaj = [];
+// let eMaj = [];
+// let fMaj = [];
+// let gMaj = [];
+// let aMaj = [];
+// let bMaj = [];
+// let cSharpMaj = [];
+// let dSharpMaj = [];
+// let fSharpMaj = [];
+// let gSharpMaj = [];
+// let aSharpMaj = [];
+// let cMin = [];
+// let dMin = [];
+// let eMin = [];
+// let fMin = [];
+// let gMin = [];
+// let aMin = [];
+// let bMin = [];
+// let cSharpMin = [];
+// let dSharpMin = [];
+// let fSharpMin = [];
+// let gSharpMin = [];
+// let aSharpMin = [];
+
+let scale1=[261.63,293.66];
+let soundLoop;
+
+let a = 0.1;
+let d = 0.7;
+let s = 0.3;
+let r = 0.1;
+let dots = []
+let columns = []
+let oscillators = [];
+let mCont = document.getElementById("matrix");
+
+let hitEvent = new Event("hit");
+
+function setup(){
+  for (var i = 0; i < cMajPentMIDI.length; i++) {
+    cMajPent.push(noteToFreq(cMajPentMIDI[i]));
+  }
+
+  class v{
+    constructor(y){
+      this.div=document.createElement("div",y.toString());
+      this.osc= new p5.Oscillator();
+      this.osc.freq(cMajPent[15-y]);
+      this.env= new p5.Envelope(a,d,s,r)
+      this.env.setInput(this.osc);
+      this.env.setRange(0.2,0)
+      this.osc.start();
+      this.div.addEventListener("click",()=>{
+
+      });
+    }
+  }
+  class voice{
+    constructor(x,y,oscillator){
+      this.enabled=0;
+      this.x=x;
+      this.y=y;
+      this.xHex=x.toString(16);
+      this.yHex=y.toString(16);
+      this.id=x.toString(16)+y.toString(16);
+      this.offset=0;
+      this.voice=oscillator;
+      // console.log(oscillators[y]);
+      this.div=document.createElement("div",x.toString());
+      this.div.className = "voice";
+      this.div.addEventListener("click",()=>{
+        // console.log(this)
+        if(this.enabled==0){
+          this.enabled=1;
+          this.div.style.backgroundColor="#00ff00";
+          oscillator.env.play();
+          render();
+          console.log(this.x+","+this.y);
+        } else if(this.enabled==1){
+          this.enabled=0;
+          this.div.style.backgroundColor="#0000ff";
+        }
+      });
+      this.div.addEventListener("hit",()=>{
+        oscillator.env.play();
+      })
+      this.div.style.backgroundColor="#0000ff";
+      mCont.appendChild(this.div);
+      this.play = ()=>{
+        // console.log("hey");
+        // if(this.enabled==true){
+          oscillator.env.play();
+        // }
+      }
+
+    }
+  }
+  for (var i = 0; i < 16; i++) {
+      columns.push([]);
+  }
+
+  for (let y=0;y<16;y++){
+    let oscillator = new v(y);
+    oscillators.push(oscillator);
+
+    for(let x=0;x<16;x++){
+      let o = new voice(x,y,oscillator)
+      columns[x].push(o);
+    }
+  }
+
+
+  let btn = document.getElementById("btn")
+  btn.addEventListener('click',()=>{
+    // oscillators.forEach((item, index) => console.log(`${index}:${item}`))
+    // console.log(oscillators)
+    for (var i = 0; i < oscillators.length; i++) {
+      oscillators[i].osc.freq(dMin[i+10]);
+      oscillators[i].env.setADSR(a,d,s,r);
+    }
+  })
+
+
+  let cnv = createCanvas(100, 100);
+  cnv.position(500,200);
+   cnv.mousePressed(canvasPressed);
+   colorMode(HSB);
+   background(0, 0, 86);
+   text('tap to start/stop', 10, 20);
+
+   //the looper's callback is passed the timeFromNow
+   //this value should be used as a reference point from
+   //which to schedule sounds
+   let intervalInSeconds = 0.2;
+   soundLoop = new p5.SoundLoop(onSoundLoop, intervalInSeconds);
+}
+
+function noteToFreq(note) {
+    let a = 440; //frequency of A (coomon value is 440Hz)
+    return (a / 32) * (2 ** ((note - 9) / 12));
+}
+
+function render(){
+  //fun background css here
+}
+
+function canvasPressed() {
+  // ensure audio is enabled
+  userStartAudio();
+
+  if (soundLoop.isPlaying) {
+    soundLoop.stop();
+  } else {
+    // start the loop
+    soundLoop.start();
+  }
+}
+
+
+function onSoundLoop(timeFromNow) {
+  let oldIndex = (Math.abs((soundLoop.iterations - 2) % columns.length));
+  let noteIndex = (soundLoop.iterations - 1) % columns.length;
+  // console.log(soundLoop.iterations+" "+noteIndex);
+
+  // columns[noteIndex].playC(play, 0.5, timeFromNow);
+  playColumn(noteIndex,oldIndex);
+  // background(noteIndex * 360 / notePattern.length, 50, 100);
+}
+let last;
+function playColumn(col,old){
+  for (var i = 0; i < columns[col].length; i++) {
+    columns[old][i].div.classList.remove("now");
+    columns[col][i].div.classList.add("now");
+    if(columns[col][i].enabled){
+      columns[col][i].play();
+
+    }
+  }
+  // for (var i = 0; i < columns[col].length; i++) {
+  //   // columns[col][i].div.classList.add("now");
+  //
+  //   if(col>10){
+  //     last = col-1
+  //     columns[last][i].div.classList.remove("now");
+  //   } else {
+  //     last=15;
+  //     columns[last][i].div.classList.remove("now");
+  //   }
+  //   console.log(last);
+  //
+  // }
+}
--- www/js/ds2.js
+++ www/js/ds2.js
@@ -1,48 +1,200 @@
 
-let synths = [];
-function preload(){
-  notes=loadJSON("js/notes.json");
 
-}
-class Synth{
-  constructor(num){
-    let t1 = 0.1; // attack time in seconds
-    let l1 = 0.7; // attack level 0.0 to 1.0
-    let t2 = 0.3; // decay time in seconds
-    let l2 = 0.1; // decay level  0.0 to 1.0
+      let synths = [];
 
-    // this.size = random(10, 20);
-    // this.color = color(random(100), 100, 100);
+      class Synth{
+        constructor(num){
+          let t1 = 0.1; // attack time in seconds
+          let l1 = 0.7; // attack level 0.0 to 1.0
+          let t2 = 0.3; // decay time in seconds
+          let l2 = 1; // decay level  0.0 to 1.0
 
-    this.env= new p5.Envelope(t1,l1,t2,l2);
-    this.osc=new p5.Oscillator('sine');
+          // this.size = random(10, 20);
+          // this.color = color(random(100), 100, 100);
 
-    this.osc.freq((Math.random()*1000)+20);
-  }
-  play(){
-    this.osc.start();
-    console.log(this);
-    this.env.play(this.osc);
-  }
-}
-function setup(){
+          this.env= new p5.Envelope(t1,l1,t2,l2);
+          this.osc=new p5.Oscillator('sine');
+          this.env.setRange(0.1,0);
 
-}
+          this.osc.freq((Math.random()*1000)+200);
 
-function draw(){
-
-}
-for (var i = 0; i < 100; i++) {
-  let e = document.createElement("div");
-  e.className="box";
-  e.id="box"+i;
-  document.body.appendChild(e);
-  let s = new Synth(i);
-  synths.push(s);
-  e.addEventListener("click",()=>{
-    s.play()
-    // console.log(e);
-  })
+        }
+        play(){
+          this.osc.start();
+          console.log(this);
+          this.env.play(this.osc);
+        }
+      }
 
 
-}
+
+
+      var container;
+			var camera, scene, raycaster, renderer;
+
+			var mouse = new THREE.Vector2(), INTERSECTED;
+			var radius = 500, theta = 0;
+			var frustumSize =1200;
+
+			init();
+			animate();
+      var listener = new THREE.AudioListener();
+      camera.add( listener );
+      var sound = new THREE.Audio( listener );
+      var audioLoader = new THREE.AudioLoader();
+      audioLoader.load( 'samples/force3.mp3', function( buffer ) {
+      	sound.setBuffer( buffer );
+      	sound.setLoop( true );
+      	sound.setVolume( 0.5 );
+      	// sound.play();
+      });
+			function init() {
+
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				var aspect = window.innerWidth / window.innerHeight;
+				camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000 );
+
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0xf000f0 );
+
+				var light = new THREE.DirectionalLight( 0xffffff, 1 );
+				light.position.set( 1, 1, 1 ).normalize();
+				scene.add( light );
+
+				var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
+
+				for ( var i = 0; i < 100; i ++ ) {
+
+					var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+					object.position.x = Math.random() * 800 - 400;
+					object.position.y = Math.random() * 800 - 400;
+					object.position.z = Math.random() * 800 - 400;
+
+					object.rotation.x = Math.random() * 2 * Math.PI;
+					object.rotation.y = Math.random() * 2 * Math.PI;
+					object.rotation.z = Math.random() * 2 * Math.PI;
+
+					object.scale.x = Math.random() +2;
+					object.scale.y = Math.random() +2;
+					object.scale.z = Math.random() +2;
+
+					scene.add( object );
+
+          let s = new Synth(i);
+          synths.push(s);
+
+				}
+
+				raycaster = new THREE.Raycaster();
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				// stats = new Stats();
+				// container.appendChild( stats.dom );
+
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+        document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function onWindowResize() {
+
+				var aspect = window.innerWidth / window.innerHeight;
+
+				camera.left = - frustumSize * aspect / 2;
+				camera.right = frustumSize * aspect / 2;
+				camera.top = frustumSize / 2;
+				camera.bottom = - frustumSize / 2;
+
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function onDocumentMouseMove( event ) {
+
+				event.preventDefault();
+
+				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+			}
+
+      function onDocumentMouseDown( event ) {
+        event.preventDefault();
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+
+      }
+			//
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+				// stats.update();
+
+			}
+
+			function render() {
+
+				theta += 1;
+
+				camera.position.x = radius * Math.sin( degtorad( theta ) );
+				camera.position.y = radius * Math.sin( degtorad( theta ) );
+				camera.position.z = radius * Math.cos( degtorad( theta ) );
+				camera.lookAt( scene.position );
+
+				camera.updateMatrixWorld();
+
+				// find intersections
+
+        raycaster.setFromCamera( mouse, camera );
+
+        var intersects = raycaster.intersectObjects( scene.children );
+
+        if ( intersects.length > 0 ) {
+
+          if ( INTERSECTED != intersects[ 0 ].object ) {
+
+            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+            INTERSECTED = intersects[ 0 ].object;
+            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+            INTERSECTED.material.emissive.setHex( 0xff0000 );
+            synths[INTERSECTED.id].play();
+
+          }
+
+        } else {
+
+          if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+          INTERSECTED = null;
+
+        }
+
+
+				renderer.render( scene, camera );
+
+			}
+
+
+      function degtorad(degrees)
+      {
+        var pi = Math.PI;
+        return degrees * (pi/180);
+      }
